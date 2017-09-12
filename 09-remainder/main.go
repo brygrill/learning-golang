cb2634baed27de5570ea19026b06f11501a2d4f6
Bryan Grill <grillb@gmail.com>
2017-09-12 14:24:24 -0400

    add remainder example

---
+++ 09-remainder/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 13 % 3
+	fmt.Println(x)
+
+	for i := 1; i <= 100; i++ {
+		if i%2 == 1 {
+			fmt.Println(i, "Odd")
+		} else {
+			fmt.Println(i, "Even")
+		}
+	}
+}
